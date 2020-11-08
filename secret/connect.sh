9c78ca5a4b7d87c51070346b9deb4ec4b3f6f471
khiluck <khiluck@gmail.com>
2020-11-08 13:41:22 +0200

    Multiple changes)

---
+++ secret/connect.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export TERM=xterm
+
+clear
+
+# Read command output line by line into array ${lines [@]}
+readarray -t lines < <(sort -k1 ~/secret/ip.list | grep "^[^#;]" | sed -e "s/[[:space:]]\+/ /g")
+
+# Prompt the user to select one of the lines.
+echo "Please select server:"
+echo "(Ctrl+C for exit)"
+echo ""
+select choice in "${lines[@]}"; do
+  [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
+  break # valid choice was made; exit prompt.
+done
+
+# Split the chosen line into ID and serial number.
+read -r SN IP PORT unused <<<"$choice"
+
+clear
+
+# If PORT variable is not empty, connect using -p option
+[[ -z $PORT ]] || { echo "Connecting to IP: [$IP]; Port: [$PORT]"; $(which ssh) -lroot $IP -p $PORT; exit; }
+echo "Connecting to IP: [$IP];"
+$(which ssh) -lroot $IP
---
+++ secret/rdpconnect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+
+# Read command output line by line into array ${lines [@]}
+readarray -t lines < <(sort -k1 ~/secret/rdp.list | grep "^[^#;]" | sed -e "s/[[:space:]]\+/ /g")
+
+# Prompt the user to select one of the lines.
+echo "Please select server:"
+echo "(Ctrl+C for exit)"
+echo ""
+select choice in "${lines[@]}"; do
+  [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
+  break # valid choice was made; exit prompt.
+done
+
+# Split the chosen line into ID and serial number.
+read -r SN IP USERNAME PASSWORD unused <<<"$choice"
+
+clear
+
+# If PORT variable is not empty, connect using -p option
+echo "Connecting to IP: [$IP];"
+#$(which xfreerdp) /u:$USERNAME /p:$PASSWORD /v:$IP /f /scale:140 /scale-desktop:140 /sound:sys:pulse /network:auto /fonts /cert:ignore +auto-reconnect +heartbeat +aero -z 
+$(which xfreerdp) /u:$USERNAME /p:$PASSWORD /v:$IP /f /smart-sizing:1366x768 /sound:sys:pulse /network:auto /fonts /cert:ignore +auto-reconnect +heartbeat +aero -z 
