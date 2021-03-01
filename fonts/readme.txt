9656546233349246c1b663b2a49320e051527da5
khiluck <khiluck@gmail.com>
2021-03-01 09:24:06 +0200

    Instructions for installation fonts in archlinux Switch to monaco plain ... love it!

---
+++ fonts/readme.txt
@@ -0,0 +1,4 @@
+font installation:
+1. copy .ttf file to /usr/share/fonts/TTF/ folder
+2. fc-cache to update the fonts cache, os can now read this font
+3. fc-list to view all fonts, and get know the fonts name. (For instance - Monaco:style=Plain)
--- st/config.h
+++ st/config.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Terminus (TTF):pixelsize=32:antialias=true:autohint=true";
-static char *font = "Monaco:pixelsize=20:antialias=true:autohint=true";
+//static char *font = "Monaco:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Monaco:style=Plain:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
