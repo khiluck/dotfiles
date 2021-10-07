970d27b9c50ffc4de868feb34740d5267213dc2d
Oleksandr Khiliuk <khiluck@gmail.com>
2021-10-07 20:53:34 +0300

    Add noisereduce script. Change environment for PC Intel i7

--- scripts/rdpconnect.sh
+++ scripts/rdpconnect.sh
@@ -37,7 +37,7 @@ done
 
 # If PORT variable is not empty, connect using -p option
 echo "Connecting to IP: [$IP];"
-$(which xfreerdp) /u:$USERNAME /p:$PASSWORD /v:$IP /drive:Downloads,/home/aex/Downloads /f /smart-sizing:1424x771 /sound:sys:pulse /network:auto /fonts /cert:ignore +auto-reconnect +heartbeat +aero -z -window-drag -menu-anims -themes +fonts -decorations +compression /audio-mode:0 /mic:format:1 /sound:latency:50 -floatbar
+$(which xfreerdp) /u:$USERNAME /p:$PASSWORD /v:$IP /drive:Downloads,/home/aex/Downloads /f /smart-sizing:1600x900 /sound:sys:pulse /network:auto /fonts /cert:ignore +auto-reconnect +heartbeat +aero -z -window-drag -menu-anims -themes +fonts -decorations +compression /audio-mode:0 /mic:format:1 /sound:latency:50 -floatbar
 
 
 
