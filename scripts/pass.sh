8108af7cc8994ef5ac5b77f1666e1fdde106a25d
Oleksandr Khiliuk <khiluck@gmail.com>
2022-01-21 00:08:26 +0200

    Add check for strong password

--- scripts/pass.sh
+++ scripts/pass.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-TEMPASS=$(< /dev/urandom tr -dc a-hj-km-np-zA-HJ-KM-NP-Z2-9@\#$%_?^\! | head -c12)
+# generate password
+newpass()
+{
+PASSOK=0
+while [ $PASSOK -ne 1 ]; do
+    TEMPASS=$(< /dev/urandom tr -dc a-hj-km-np-zA-HJ-KM-NP-Z2-9@\#$%_?^\! | head -c12)
+    PASSOK=1
+    # verify that there is at least one lower case letter
+    [ "${TEMPASS}" = "${TEMPASS^^}" ] && { PASSOK=0; } # echo "No lower case letter"; }
+    [ "${TEMPASS}" = "${TEMPASS,,}" ] && { PASSOK=0; } # echo "No upper case letter"; }
+    [ "${TEMPASS//[^0-9]/}" ] || { PASSOK=0; } # echo "No number"; }
+    [ "${TEMPASS//[^@\#$%_?^\!]/}" ] || { PASSOK=0; } # echo "No special symbol!"; }
+done
+echo $TEMPASS
+}
+
+
+TEMPASS=$(newpass)
 echo $TEMPASS | xclip -r -l 1 
 echo $TEMPASS | xclip -selection clipboard
 TEMPASS=""
